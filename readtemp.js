f8939ba6203e75247791927f4f4ff4599c38f39a
Roger Larsen <roger.larsen@gmail.com>
2013-09-21 03:19:07 +0000

    made it interval based

--- readtemp.js
+++ readtemp.js
@@ -1,4 +1,6 @@
 var exec = require('child_process').exec;
+//var readline = require('readline');
+//var sleep = require('sleep');
 
 var parseTempF = function(str) {
     return (((parseInt(str, 16) / 50 - 273.15) * 1.8 + 32).toFixed(2));
@@ -10,10 +12,8 @@ var parseTempC = function(str) {
 var cm = 'i2cget -y 1 0x5a 0x07 w';
 console.log('getting temp..');
 
-function readt() { 
+function readt() {
 	exec(cm, function(err, stdout, stderr) {
-	//console.log(stdout);
-	//console.log(stderr);
 	if(err !== null) {
 		console.log('error: ' + err);	
 	} else if(stdout !== null) {
@@ -21,7 +21,15 @@ function readt() {
 	}
 	});
 }
+//readt();
+//for(var x = 0; x < 1000; x++) {
+//	readt();
+//}
+//setInterval(readt(), 500);
+//while(true) {
+//	readt();
+	//sleep.sleep(1);
+//	break;
+//}
 
-for(var x = 0; x < 10; x++) {
-	readt();
-}
+setInterval(function() { readt(); }, 500);
